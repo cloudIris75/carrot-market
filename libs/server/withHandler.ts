6e47999297e460576c5f01070ea6ffc6907580b9
cloudIris75 <gmyeom1230@gmail.com>
2022-05-31 19:08:46 +0900

    Token logic

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,7 +1,12 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export interface ResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
 
 export default function withHandler(
-  method: "GET" | "POST" | "DELETE",
+  method: 'GET' | 'POST' | 'DELETE',
   fn: (req: NextApiRequest, res: NextApiResponse) => void
 ) {
   return async function (req: NextApiRequest, res: NextApiResponse) {
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,11 +1,15 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "@libs/server/withHandler";
-import client from "@libs/server/client";
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const user = phone ? { phone: +phone } : { email };
-  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + '';
   const token = await client.token.create({
     data: {
       payload,
@@ -15,51 +19,16 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
             ...user,
           },
           create: {
-            name: "Anonymous",
+            name: 'Anonymous',
             ...user,
           },
         },
       },
     },
   });
-  console.log(token);
-  /* if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,
-      },
-    });
-    if (user) console.log("found it.");
-    if (!user) {
-      console.log("Did not find. Will create.");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        },
-      });
-    }
-    console.log(user);
-  } */
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
-export default withHandler("POST", handler);
+export default withHandler('POST', handler);
