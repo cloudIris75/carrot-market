75dd591d787037a8a0b00ec400925b8120201033
cloudIris75 <gmyeom1230@gmail.com>
2022-05-31 15:46:41 +0900

    Modify files

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,38 +1,11 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { SWRConfig } from "swr";
-import Script from "next/script";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log("APP IS RUNNING");
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) =>
-          fetch(url).then((response) => response.json()),
-      }}
-    >
-      <div className="w-full max-w-xl mx-auto">
-        <Component {...pageProps} />
-      </div>
-      {/*       <Script
-        src="https://developers.kakao.com/sdk/js/kakao.js"
-        strategy="lazyOnload"
-      />
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        onLoad={() => {
-          window.fbAsyncInit = function () {
-            FB.init({
-              appId: "your-app-id",
-              autoLogAppEvents: true,
-              xfbml: true,
-              version: "v13.0",
-            });
-          };
-        }}
-      /> */}
-    </SWRConfig>
+    <div className="w-full max-w-xl mx-auto">
+      <Component {...pageProps} />
+    </div>
   );
 }
 
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,6 @@
+import client from '@libs/server/client';
+import withHandler from '@libs/server/withHandler';
 import { NextApiRequest, NextApiResponse } from 'next';
-import client from '../../../libs/server/client';
-import withHandler from '../../../libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
