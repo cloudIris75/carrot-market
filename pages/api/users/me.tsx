7e1a24953f7a3e189dfc2e7289ff79b4a9982c76
cloudIris75 <gmyeom1230@gmail.com>
2022-06-01 16:51:54 +0900

    Clean authentication code

---
+++ pages/api/users/me.tsx
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
+import client from '@libs/server/client';
+import { withApiSession } from '@libs/client/withSession';
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const profile = await client.user.findUnique({
+    where: { id: req.session.user?.id },
+  });
+  res.json({
+    ok: true,
+    profile,
+  });
+}
+
+export default withApiSession(withHandler('GET', handler));
