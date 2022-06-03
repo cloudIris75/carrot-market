c0574d09b6513ca8a6745253639c6af196631af7
cloudIris75 <gmyeom1230@gmail.com>
2022-06-03 17:23:18 +0900

    Count relationships

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,13 @@ import Head from 'next/head';
 import useSWR from 'swr';
 import { Product } from '@prisma/client';
 
+interface ProductWithCount extends Product {
+  _count: { favs: number };
+}
+
 interface ProductsResponse {
   ok: boolean;
-  products: Product[];
+  products: ProductWithCount[];
 }
 
 const Home: NextPage = () => {
@@ -28,7 +32,7 @@ const Home: NextPage = () => {
             title={product.name}
             price={product.price}
             comments={1}
-            hearts={1}
+            hearts={product._count.favs}
           />
         ))}
         <FloatingButton href="/products/upload">
