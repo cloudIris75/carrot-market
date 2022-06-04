78f1d97e35ae79e419c9789dba91dad34b5a269e
cloudIris75 <gmyeom1230@gmail.com>
2022-06-04 20:40:46 +0900

    Cloudflare upload

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -37,18 +37,35 @@ const EditProfile: NextPage = () => {
   }, [user, setValue]);
   const [editProfile, { data, loading }] =
     useMutation<EditProfileResponse>(`/api/users/me`);
-  const onValid = ({ email, phone, name, avatar }: EditProfileForm) => {
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === '' && phone === '' && name === '') {
       return setError('formErrors', {
         message: 'Email OR Phone number are required. You need to choose one.',
       });
     }
-    editProfile({
-      email,
-      phone,
-      name,
-    });
+    if (avatar && avatar.length > 0 && user) {
+      const { id, uploadURL } = await (await fetch(`/api/files`)).json();
+      const form = new FormData();
+      form.append('file', avatar[0], user?.id + '');
+      await fetch(uploadURL, {
+        method: 'POST',
+        body: form,
+      });
+      return;
+      editProfile({
+        email,
+        phone,
+        name,
+        // avatarUrl: CF URL
+      });
+    } else {
+      editProfile({
+        email,
+        phone,
+        name,
+      });
+    }
   };
   useEffect(() => {
     if (data && !data.ok && data.error) {
