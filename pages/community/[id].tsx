73d83982639cd8b0102d306bbdd2f7f23cc97dfc
cloudIris75 <gmyeom1230@gmail.com>
2022-06-07 12:15:53 +0900

    Check codes

--- pages/community/[id].tsx
+++ pages/community/[id].tsx
@@ -157,7 +157,7 @@ const CommunityPostDetail: NextPage = () => {
                   {answer.user.name}
                 </span>
                 <span className="text-xs text-gray-500 block ">
-                  {String(answer.createdAt)}
+                  {answer.createdAt}
                 </span>
                 <p className="text-gray-700 mt-2">{answer.answer} </p>
               </div>
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -6,7 +6,6 @@ import useSWR from 'swr';
 import { Post, User } from '@prisma/client';
 import useCoords from '@libs/client/useCoords';
 import client from '@libs/server/client';
-
 interface PostWithUser extends Post {
   user: User;
   _count: {
@@ -41,7 +40,7 @@ const Community: NextPage<PostsResponse> = ({ posts }) => {
               </div>
               <div className="mt-5 px-4 flex items-center justify-between w-full text-gray-500 font-medium text-xs">
                 <span>{post.user.name}</span>
-                <span>{String(post.createdAt)}</span>
+                <span>{post.createdAt}</span>
               </div>
               <div className="flex px-4 space-x-5 mt-3 text-gray-700 py-2.5 border-t   w-full">
                 <span className="flex space-x-2 items-center text-sm">
